60cd5aa583eeec0ef8c20330a6b2d55962324760
derowon <derowon@gmail.com>
2016-05-26 18:13:54 -0300

    Initial Commit

---
+++ Vidrios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Vidrios
+//
+//  Created by Dero on 2016. 5. 26..
+//  Copyright © 2016년 Dero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Vidrios/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Vidrios
+//
+
+//  Copyright (c) 2016년 Dero. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
